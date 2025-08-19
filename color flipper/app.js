70eb100ddfa5809d2319d8c69955a41d21bc084d
kaiyeolbaek <kaiyeolbaek8422@gmail.com>
2025-08-19 21:29:53 +0630

    click button , change background from array list

--- color flipper/app.js
+++ color flipper/app.js
@@ -1,1 +1,28 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+// console.log(colors);
+const button = document.getElementById("btn");
+// console.log(button)
+const color = document.querySelector(".color")
+// console.log(color)
+
+
+// For One Random Number Color Only
+// const oneRandomColor = (colors) =>{
+//     const randomNumber = 2;
+//     document.body.style.backgroundColor = colors[2];
+//     color.textContent = colors[2]
+// }
+
+// button.addEventListener('click', () => oneRandomColor(colors))
+
+let currentindex = 0;
+
+button.addEventListener('click' , function (){
+    document.body.style.backgroundColor = colors[currentindex];
+    color.textContent = colors[currentindex]
+    currentindex++;
+
+    if(currentindex >= colors.length){
+        currentindex = 0;
+    }
+})
