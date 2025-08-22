8e455b3d808a6504b0ff1a19b9fdd8f50788d8e3
kaiyeolbaek <kaiyeolbaek8422@gmail.com>
2025-08-22 21:26:04 +0630

    count challenge(js) with my own idea

---
+++ counter/app.js
@@ -0,0 +1,38 @@
+const decrease = document.getElementById('decrease');
+const reset = document.getElementById('reset');
+const increase = document.getElementById('increase');
+
+let num = document.querySelector('#value');
+let cut = 0;
+
+function col() {
+    if (cut == 0) {
+        document.getElementById('value').style.color = "black";
+    } else if (cut > 0) {
+        document.getElementById('value').style.color = "green";
+    } else {
+        document.getElementById('value').style.color = "red";
+    }
+}
+
+increase.addEventListener('click', function () {
+    let result = ++cut;
+    cut = result;
+    num.textContent = cut;
+    col();
+})
+
+reset.addEventListener('click', function () {
+    let result = 0;
+    cut = result;
+    num.textContent = cut;
+    col();
+})
+
+decrease.addEventListener('click', function () {
+    let result = --cut;
+    cut = result;
+    num.textContent = cut;
+    col();
+})
+
