5136f745c32c781e2eb681944047710d282cbc7e
kaiyeolbaek <kaiyeolbaek8422@gmail.com>
2025-08-27 22:05:44 +0630

    nav bar challenge (javascript) file upload

--- nav-bar/app.js
+++ nav-bar/app.js
@@ -3,3 +3,24 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const navToggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links')
+
+// console.log(navToggle);
+// console.log(links);
+
+navToggle.addEventListener('click',function (){
+    // console.log(links.classList);
+    // console.log(links.classList.contains('random'));
+    // console.log(links.classList.contains('links'));
+
+    // if(links.classList.contains('show-links')){
+    //     links.classList.remove('show-links');
+    // }
+    // else{
+    //     links.classList.add('show-links');
+    // }
+
+    links.classList.toggle('show-links');
+})
