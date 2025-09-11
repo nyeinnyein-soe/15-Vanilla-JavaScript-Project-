1c228535a3cd19c36f8c741cb53a346307a4cf1f
kaiyeolbaek <kaiyeolbaek8422@gmail.com>
2025-09-11 22:09:34 +0630

    questions challenge from teacher's lecture

--- questions/app.js
+++ questions/app.js
@@ -1,9 +1,11 @@
 //using selectors inside the element
 // traversing the dom
 
-const allPlusBtn = document.querySelectorAll('.plus-icon');
-// console.log(plusBtn);
-const minusBtn = document.querySelectorAll('.minus-icon');
+// My Solution
+
+// const plusBtn = document.querySelector('.plus-icon');
+// // console.log(plusBtn);
+// const minusBtn = document.querySelector('.minus-icon');
 
 const questions = document.querySelectorAll('.question')
 
@@ -13,7 +15,7 @@ const questionBtn = document.querySelectorAll('.question-btn');
 // console.log(plusBtn);
 // console.log(minusBtn);
 // console.log(questionText);
-console.log(questionBtn);
+// console.log(questionBtn);
 
 // allPlusBtn.forEach(function(btn) {
 //     btn.addEventListener('click',function(){
@@ -33,22 +35,37 @@ console.log(questionBtn);
 //     plusBtn.style.display = "inline";
 // })
 
-questions.forEach(function(question) {
-    let plusBtn = question.querySelector('.plus-icon');
-    let minusBtn = question.querySelector('.minus-icon');
-    let questionText = question.querySelector('.question-text');
+// questions.forEach(function(question) {
+//     let plusBtn = question.querySelector('.plus-icon');
+//     let minusBtn = question.querySelector('.minus-icon');
+//     let questionText = question.querySelector('.question-text');
 
-    plusBtn.addEventListener('click',function(){
-        questionText.classList.add('show-text');
-        questionText.classList.remove('question-text');
-        minusBtn.classList.remove('minus-icon');
-        plusBtn.style.display = "none";
-    })
+//     plusBtn.addEventListener('click',function(){
+//         questionText.classList.add('show-text');
+//         questionText.classList.remove('question-text');
+//         minusBtn.classList.remove('minus-icon');
+//         plusBtn.style.display = "none";
+//     })
 
-    minusBtn.addEventListener('click',function(){
-        questionText.classList.remove('show-text');
-        questionText.classList.add('question-text');
-        minusBtn.classList.add('minus-icon');
-        plusBtn.style.display = "inline";
-    })
-})
+//     minusBtn.addEventListener('click',function(){
+//         questionText.classList.remove('show-text');
+//         questionText.classList.add('question-text');
+//         minusBtn.classList.add('minus-icon');
+//         plusBtn.style.display = "inline";
+//     })
+// })
+
+// From Teacher's lecture
+
+// const btns = document.querySelectorAll('.question-btn');
+// // console.log(btns);
+
+// btns.forEach(function (btn){
+//     // console.log(btn);
+//     btn.addEventListener('click',function(e){
+//         // console.log(e.currentTarget.parentElement.parentElement);
+//         const question = e.currentTarget.parentElement.parentElement;
+//         question.classList.toggle('show-text');
+//     })
+// }
+// )
