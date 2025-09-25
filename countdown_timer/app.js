36023b32d3efaceee98e659119ca31d72bdd8c67
kaiyeolbaek <kaiyeolbaek8422@gmail.com>
2025-09-25 16:41:43 +0630

    create html and use new Date() in js

--- countdown_timer/app.js
+++ countdown_timer/app.js
@@ -21,3 +21,17 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector('.giveaway');
+const deadline = document.querySelector('.deadline');
+const items = document.querySelectorAll('.deadline-format h4');
+// console.log(items)
+
+let futureDate = new Date(2024, 3, 8, 9, 30); // Year , Month , Date , Hours , Minutes
+// console.log(futureDate);
+
+const year = futureDate.getFullYear();
+const hour = futureDate.getHours();
+const minute = futureDate.getMinutes();
+
+giveaway.textContent = `giveaway ends on ${year} ${hour}:${minute}am`;
